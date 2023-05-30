5ec0d589907576864a74f83ba3fdbd272f951e4c
Cesar Herrera <cesarherr321@gmail.com>
2023-05-29 23:14:08 -0400

    Add imported component to top-level

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ function App() {
   return (
     <div className="App">
       <Calculator
+      name1="/"
+      name2="%"
+      name3="+/-"
+      name4="AC"
+      name5="X"
+      name6={9}
+      name7={8}
+      name8={7}
+      name9="-"
+      name10={6}
+      name11={5}
+      name12={4}
+      name13="+"
+      name14={3}
+      name15={2}
+      name16={1}
+      name17="="
+      name18="."
+      name19={0}
       
       />
     </div>
