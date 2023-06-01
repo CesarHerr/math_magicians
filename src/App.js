c046fb00ab32ace5168498135e8d22b03d1f1b46
Cesar Herrera <cesarherr321@gmail.com>
2023-06-01 11:56:49 -0400

    Add calculate and operate files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css';
-import Calculator from './components/calculator';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator
-        name1="÷"
-        name2="%"
-        name3="+/-"
-        name4="AC"
-        name5="X"
-        name6={9}
-        name7={8}
-        name8={7}
-        name9="-"
-        name10={6}
-        name11={5}
-        name12={4}
-        name13="+"
-        name14={3}
-        name15={2}
-        name16={1}
-        name17="="
-        name18="."
-        name19={0}
-
-      />
-    </div>
+    <section className="App">
+      <Calculator />
+    </section>
   );
 }
 
