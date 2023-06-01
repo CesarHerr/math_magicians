61c348d2246b509449bacf7f5b2d73e948907bad
Cesar Herrera <cesarherr321@gmail.com>
2023-06-01 12:04:46 -0400

    Add calculate to calculator function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -18,5 +18,56 @@ function Display(props) {
   );
 }
 
+const allButtons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
+function Calculator() {
+  const [onDisplay, setOnDisplay] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const setClass = (name) => {
+    let btnClass = 'btn';
+    if (name === '÷' || name === 'x' || name === '-' || name === '+' || name === '=') {
+      btnClass = 'btn orange';
+    } else if (name === '0') {
+      btnClass = 'btn zero';
+    }
+
+    return btnClass;
+  };
+
+  const handleButton = (name) => {
+    setOnDisplay(calculate(onDisplay, name));
+  };
+
+  return (
+    <div className="calculator-container">
+      <Display result={onDisplay.next || onDisplay.total || '0'} />
+      {
+        allButtons.map((name) => (
+          <Buttons
+            onClick={handleButton}
+            key={name}
+            name={name}
+            setClass={setClass}
+
+          />
+        ))
+      }
+    </div>
+  );
+}
+
+Buttons.propTypes = {
+  name: PropTypes.string.isRequired,
+  setClass: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+Display.propTypes = {
+  result: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+};
 
 export default Calculator;
